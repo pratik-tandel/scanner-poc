92b1488ec9a7c8ddddd54944a22a9be3bd2a2825
Pratik Tandel <pratik.tandel@1rivet.com>
2024-09-25 19:30:45 +0530

    feat: add routing and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
+      <div>
+        <Link to="/html5-qrcode" className="link">Html5 QrCode</Link>
+        <Link to="/react-zxing" className="link">React ZXing</Link>
+      </div>
       <Outlet />
     </div>
   )
